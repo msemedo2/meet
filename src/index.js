952cef07afffe01414b877c596c42a50acf2c800
unknown <miguel.semedo.2@outlook.com>
2022-08-18 10:35:41 +0100

    integrate atatus

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('fb76c4f2eb81470cb05a3d63db9cee2e').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
